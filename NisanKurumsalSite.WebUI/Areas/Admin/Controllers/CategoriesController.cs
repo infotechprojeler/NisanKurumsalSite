1640e4ceed68bfad29918b7a2babdcef536e50d2
InfotechProjeler <ilica-18@hotmail.com>
2024-09-28 14:33:07 +0300

    ProductsController a geldik

---
+++ NisanKurumsalSite.WebUI/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NisanKurumsalSite.Data;
+using NisanKurumsalSite.Entities;
+
+namespace NisanKurumsalSite.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public CategoriesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CategoriesController
+        public ActionResult Index()
+        {
+            return View(_context.Categories);
+        }
+
+        // GET: CategoriesController/Create
+        public ActionResult Create()
+        {
+            ViewBag.ParentId = new SelectList(_context.Categories, "Id", "Name"); // ekrandaki kategori seçim elemanına veritabanındaki kategorileri gönderiyoruz.
+            return View();
+        }
+
+        // POST: CategoriesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Category collection)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Categories.Add(collection);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            ViewBag.ParentId = new SelectList(_context.Categories, "Id", "Name");
+            return View(collection);
+        }
+
+        // GET: CategoriesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ViewBag.ParentId = new SelectList(_context.Categories, "Id", "Name");
+            var model = _context.Categories.Find(id);
+            return View(model);
+        }
+
+        // POST: CategoriesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Category collection)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Categories.Update(collection);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            ViewBag.ParentId = new SelectList(_context.Categories, "Id", "Name");
+            return View(collection);
+        }
+
+        // GET: CategoriesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var model = _context.Categories.Find(id);
+            return View(model);
+        }
+
+        // POST: CategoriesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Category collection)
+        {
+            try
+            {
+                _context.Categories.Remove(collection);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ NisanKurumsalSite.WebUI/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NisanKurumsalSite.Data;
+
+namespace NisanKurumsalSite.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public ProductsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProductsController
+        public ActionResult Index()
+        {
+            return View(_context.Products);
+        }
+
+        // GET: ProductsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
